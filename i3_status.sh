df9c3e4f5dc4d60780ef3d6bbe600ea10fac5487
Sergei-Korneev <60899505+Sergei-Korneev@users.noreply.github.com>
2022-09-10 21:51:55 +0500

    Update i3_status.sh

--- i3_status.sh
+++ i3_status.sh
@@ -1,22 +1,20 @@
-#!/bin/bash
-  2 
-  3 
-  4 interval=3
-  5 
-  6 while true; do (
-  7 
-  8          topcpu="TopCpu: $(ps -eo cmd,%cpu --sort=-%cpu |  sed -n 2p)"
-  9          timedate="$(date +'%a %d %b %Y %H:%M')"
- 10          svolume="$(pactl  get-sink-volume $(pactl list short sinks |  awk '{print $1}' )  | sed -n 1p|  awk '{print "vol: R" $5 " L" $12}')"
- 11          mem="$(free | sed -n 2p | awk '{printf "%s%.0f%s%.0f\n" ,  "mem: ",$3/1024,"/",$7/1024}')"
- 12          cputemp="Temp: $(sensors 2> /dev/null | grep 'Package id 0:'  | awk '{print $4}')"
- 13          cpu="CPU: $(top -bn2 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,'|awk '{print 100-$8 "%"}')"
- 14          battery="Battery: $(upower -i $(upower -e | grep BAT) | grep  -E "state|to\ full|to\ empty|percentage" |  tr -d '\n' | sed 's/  */ /g')"
- 15          ineta=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'|  tr  '\n' ' ')
- 16         clear
- 17         echo "$cpu | $topcpu | $cputemp | $battery | $mem | $ineta | $svolume | $timedate"
- 18  sleep $interval)
- 19 done
-~
-~
-~
+ #!/bin/bash
+  
+  
+  interval=3
+  
+  while true; do (
+           
+           topcpu="$(ps -eo cmd,%cpu --sort=-%cpu |  sed -n 2p)"
+           timedate="$(date +'%a %d %b %Y %H:%M')"
+           svolume="$(pactl  get-sink-volume $(pactl list short sinks |  awk '{print $1}' )  | sed -n 1p|  awk '{print "R" $5 " L" $12}')"
+           mem="$(free | sed -n 2p | awk '{printf "%s%.0f%s%.0f Mb\n" ,  "",$3/1024,"/",$7/1024}')"
+           cputemp="$(sensors 2> /dev/null | grep 'Package id 0:'  | awk '{print $4}')"
+           cpu="$(top -bn2 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,'|awk '{print 100-$8 "%"}')"
+           battery="$(upower -i $(upower -e | grep BAT) | grep  -E "state|to\ full|to\ empty|percentage" | sed 'N;s/\n/,/;s/  */ /g')"
+           ineta=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'|  sed 'N;s/\n/, /;s/  */ /g')
+          clear
+          # https://www.compart.com/en/unicode
+          echo -en "\xF0\x9D\x84\x9C $cpu    \xF0\x9F\x8C\xA1 $cputemp   \xE2\x8F\xB1 $topcpu   \xF0\x9F\x99\xBE $mem   \xF0\x9F\x94\x8B $battery   \xF0\x9F\x96\xA7 $ineta    \xF0\x9F\x8E    \xA7 $svolume   $timedate"
+   sleep $interval)
+ done
